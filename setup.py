676bc186a87048eadf40fbb5d56186656d621772
sergei <adrian.illumina@gmail.com>
2024-10-03 16:58:26 +0700

    new file:   LICENSE 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import distutils.core
+
+setup(
+    name='tasty-modkit',
+    version='0.1.0',
+    author='Lovell D\'Arce',
+    author_email='',  # Leave blank as per your input
+    description='A modkit for synchronizing workshop mods with non-steam games.',
+    long_description='',  # Leave blank as per your input
+    url='https://github.com/antl3r/tasty-modkit',
+    license='MIT',
+    packages=['tasty-modkit.modkit'],
+    install_requires=[
+        'configparser',
+        'requests',
+    ],
+)
